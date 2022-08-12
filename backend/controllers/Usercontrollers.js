eb7a31748c3204808f21858034e6ce1d034a4c55
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-08-12 17:14:29 +0530

    made usermodel and connected to database firsttime

--- backend/controllers/Usercontrollers.js
+++ backend/controllers/Usercontrollers.js
@@ -1,10 +1,35 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/Usermodel");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
-  res.json({
+  const userexists = await User.findOne({ email });
+
+  if (userexists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
     name,
     email,
+    password,
+    pic,
   });
-};
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!");
+  }
+});
 
 module.exports = { registerUser };
---
+++ backend/models/Usermodel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userschema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isAdmin: {
+      // just if we wanted
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userschema);
+
+module.exports = User;
